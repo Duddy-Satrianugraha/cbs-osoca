b0424ed689f74bb465385644546f31a530a692d9
duddy_on_wsl_meu <biolibium@gmail.com>
2025-08-27 18:09:01 +0700

    tau

--- app/Http/Controllers/OsocaController.php
+++ app/Http/Controllers/OsocaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Opeserta;
 use App\Models\Oujian;
 use App\Models\Ostation;
+use App\Models\Osesi;
+use App\Models\Otemplate;
 use Illuminate\Http\Request;
 
 class OsocaController extends Controller
@@ -33,7 +35,24 @@ class OsocaController extends Controller
     }
 
     public function mhs_check(Request $request)
-    {
+    {   $request->validate([
+        'sesi-qr' => 'required',
+            ]);
+        $qr = $request->input('sesi-qr');
+
+        $peserta = Opeserta::where('qrpeserta', $qr)->where('oujian_id', session('Osoca'))->where('station', session('Station'))->first();
+       // dd($peserta);
+        if(!$peserta){
+            return redirect(route('osoca.mhs.login'))->with('msg','danger-Peserta salah masuk Ruangan atau tidak terdaftar');
+        }
+
+        if($peserta->sesi != session('current')){
+            return redirect(route('osoca.mhs.login'))->with('msg','danger-Peserta Tidak Sesuai Urutan');
+        }
+        dd($peserta);
+        $sesi = Osesi::where('oujian_id', $peserta->oujian_id)->where('urutan', $peserta->sesi)->first();
+        $template = Otemplate::find($sesi->otemplate_id);
+        $rubrik = $template->rubrix;
         dd($request->all());
     }
 }
